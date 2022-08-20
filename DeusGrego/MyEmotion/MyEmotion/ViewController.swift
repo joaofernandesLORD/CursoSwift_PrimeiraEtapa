681d8e98d06b69ac542a8aa7e15f68569250ad75
LordPinguim <jf.lipeto@gmail.com>
2022-08-20 14:28:51 -0400

    atualização do MyEmotion

--- DeusGrego/MyEmotion/MyEmotion/ViewController.swift
+++ DeusGrego/MyEmotion/MyEmotion/ViewController.swift
@@ -10,18 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var Fantasma: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func ButTrocarImagem(_ sender: Any) {
-        if (Fantasma.image == UIImage(named: "fantasma")){
-            Fantasma.image = UIImage(named: "pacman")
-        }else {
-            Fantasma.image = UIImage(named: "fantasma")
-        }
+    
+    @IBAction func happy(_ sender: Any) {
+       Fantasma.image = UIImage(named: "pingufeliz")
     }
     
+    @IBAction func sad(_ sender: Any) {
+        Fantasma.image = UIImage(named: "pingutriste")
+    }
+    
+    @IBAction func angry(_ sender: Any) {
+        Fantasma.image = UIImage(named: "pinguraiva")
+    }
+    
+    @IBAction func pingu(_ sender: Any) {
+        Fantasma.image = UIImage(named: "pingu")
+    }
+    //@IBAction func ButTrocarImagem(_ sender: Any) {
+        //if (Fantasma.image == UIImage(named: "")){
+            //Fantasma.image = UIImage(named: "")
+        //}else {
+           // Fantasma.image = UIImage(named: "")
+        //}
+    //}
 }
 

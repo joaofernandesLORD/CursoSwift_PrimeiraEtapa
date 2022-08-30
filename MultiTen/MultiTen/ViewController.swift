18ab4849b078adcdfea291ea448827d7bd271493
ICMMAC07-13C3 <icmmac07-13c3@ICMMAC07-13C3s-Mac-mini.local>
2022-08-30 16:20:54 -0400

    Arrumando GitHub

--- MultiTen/MultiTen/ViewController.swift
+++ MultiTen/MultiTen/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Num: UITextField!
+    //Local onde coloca os numeros
+    @IBOutlet weak var numero: UILabel!
+    //Onde o numero aparece
+    @IBOutlet weak var Sensei: UIImageView!
+    //sensei
+    @IBOutlet weak var TextoLegal: UILabel!
+    //Digite o numero para descobrir um segredo
+    
+    //@IBOutlet weak var Calc: UIButton!
+    //ButSegredo
+    
+    @IBOutlet weak var SecondView: UIView!
+    //Minha segunda tela
+    
+        var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButCalc(_ sender: Any) {
+        numero.text = Num.text
+        count = Int(numero.text!) ?? 0
+        
+        Num.text = ""
+        
+        SecondView.isHidden = false
+        multTen()
+        view.endEditing(true)
+    }
+    
+    @IBAction func CloseView(_ sender: Any) {
+        SecondView.isHidden = true
+        TextoLegal.text = "Digite um número"
+    }
+    
+    @IBAction func Plus(_ sender: Any) {
+        count += 1
+        numero.text = "\(count)"
+        multTen()
+    }
+    
+    @IBAction func Minus(_ sender: Any) {
+        count -= 1
+        numero.text = "\(count)"
+        multTen()
+    }
+    
+    func multTen(){
+        if (count % 10 == 0){
+            TextoLegal.text = "Ele é múltiplo de 10"
+            Sensei.image = UIImage(named: "Sensei10")
+        }else {
+            TextoLegal.text = "Contando..."
+            Sensei.image = UIImage(named: "Sensei")
+        }
+    }
+    
 }
 

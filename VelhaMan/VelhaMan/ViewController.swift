d343a62e6ebc9dd04b55ced276cd84e1551e2eeb
ICMMAC03-BEBA <icmmac03-beba@ICMMAC03-BEBAs-Mac-mini.local>
2022-09-08 18:52:07 -0400

    fazendo o visual do VelhaMan

---
+++ VelhaMan/VelhaMan/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  VelhaMan
+//
+//  Created by ICMMAC03-BEBA on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //BOTÕES DA COLUNA A
+    @IBOutlet weak var A1: UIButton!
+    @IBOutlet weak var A2: UIButton!
+    @IBOutlet weak var A3: UIButton!
+    //BOTÕES DA COLUNA B
+    @IBOutlet weak var B1: UIButton!
+    @IBOutlet weak var B2: UIButton!
+    @IBOutlet weak var B3: UIButton!
+    //BOTÕES DA COLUNA C
+    @IBOutlet weak var C1: UIButton!
+    @IBOutlet weak var C2: UIButton!
+    @IBOutlet weak var C3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

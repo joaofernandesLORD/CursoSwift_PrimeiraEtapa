7cfe4fca7f8384ef03b0e1c0f84b61dfc9fcdff9
ICMMAC07-13C3 <icmmac07-13c3@ICMMAC07-13C3s-Mac-mini.local>
2022-09-16 18:52:25 -0400

    Desafio fatorial com fibonacci

---
+++ Math Versus/Math Versus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math Versus
+//
+//  Created by ICMMAC07-13C3 on 16/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

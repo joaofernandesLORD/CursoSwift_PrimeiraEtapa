12341cebb04757ed92c5236360ba64efa6773613
ICMMAC07-13C3 <icmmac07-13c3@ICMMAC07-13C3s-Mac-mini.local>
2022-09-19 18:46:57 -0400

    Math o aplicativo de fatorial com fiboncci

---
+++ Math/Math/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math
+//
+//  Created by ICMMAC07-13C3 on 19/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
